7f5abb2e9119bf1fecb17803f88bf99e373a2a2e
Marina Drugina <druginamarina@gmail.com>
2022-09-25 00:53:55 +0300

    Postreview fixing task #2.3.1&2.3.2

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,22 +1,23 @@
 const tasksInput = document.getElementById('task__input');
 const tasksList = document.getElementById('tasks__list');
+const taskForm = document.getElementById('tasks__form');
 
-tasksInput.addEventListener('keyup', (event) => {
+taskForm.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  if (event.code === 'Enter' && tasksInput.value !== '') {
-
+  if (tasksInput.value.trim() !== '') {
     let elem = `<div class="task">
                   <div class="task__title">${tasksInput.value}</div>
                   <a href="#" class="task__remove">&times;</a>
                 </div>`;
+    
     tasksList.insertAdjacentHTML('beforeEnd', elem);
 
     tasksList.lastElementChild.lastElementChild.addEventListener('click', (event) => {
         event.preventDefault();
         event.currentTarget.parentNode.remove();
-      })
+    });
 
     tasksInput.value = '';
-  }
+   }
 });
--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,24 +1,33 @@
 const hasTooltip = document.querySelectorAll('.has-tooltip');
 
 for(let i = 0; i < hasTooltip.length; i++) {
-    hasTooltip[i].insertAdjacentHTML('afterbegin', '<div class="tooltip"></div>');
+    hasTooltip[i].insertAdjacentHTML('afterend', '<div class="tooltip"></div>');
+}
 
-    hasTooltip[i].firstElementChild.textContent = hasTooltip[i].getAttribute('title');
+const tooltip = document.querySelectorAll('.tooltip');
 
-    hasTooltip[i].addEventListener('click', (event) => {
+for(let i = 0; i < tooltip.length; i++) {
+    tooltip[i].textContent = tooltip[i].previousElementSibling.getAttribute('title');
+
+    let coords = tooltip[i].previousElementSibling.getBoundingClientRect();
+    tooltip[i].style.position = 'absolute';
+    tooltip[i].style.top = (coords.top + tooltip[i].previousElementSibling.offsetHeight + window.scrollY) + 'px';
+    tooltip[i].style.left = coords.left + 'px';
+
+    tooltip[i].previousElementSibling.addEventListener('click', (event) => {
         event.preventDefault();
 
-            let target = document.querySelector('.tooltip_active');
-            let arr = Array.from(document.querySelectorAll('.tooltip'));
-            let index = arr.indexOf(target);
+        let arr = Array.from(tooltip);
+        let target = document.querySelector('.tooltip_active');
+        let index = arr.indexOf(target);
 
-            if(index !== -1 && index !== i) {
-                hasTooltip[index].firstElementChild.classList.remove('tooltip_active');
-                hasTooltip[i].firstElementChild.classList.add('tooltip_active');
-            } else if (index !== -1 && index === i) {
-                hasTooltip[i].firstElementChild.classList.remove('tooltip_active');
-            } else {
-                hasTooltip[i].firstElementChild.classList.toggle('tooltip_active');
-            }
+        if(index !== -1 && index !== i) {
+            tooltip[index].classList.remove('tooltip_active');
+            tooltip[i].classList.add('tooltip_active');
+        } else if (index !== -1 && index === i) {
+            tooltip[i].classList.remove('tooltip_active');
+        } else {
+            tooltip[i].classList.toggle('tooltip_active');
+        }   
     });
 }
